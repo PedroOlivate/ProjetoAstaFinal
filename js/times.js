5ce7bde93a5ef4e22ea3ef3fb321fe29ce91e57a
PedroOlivate <pedroolivatebatista@gmail.com>
2022-10-10 05:09:10 -0300

    fist commit

---
+++ js/times.js
@@ -0,0 +1,70 @@
+var btn = document.querySelector('#show-or-hide');
+var container = document.querySelector('.container');
+
+btn.addEventListener('click', function() {
+
+    if(container.style.display === 'block'){
+        container.style.display = 'none';
+    }
+    else{
+        container.style.display = 'block';
+    }
+
+}); 
+
+var btn = document.querySelector('#show-or-hide2');
+var container2 = document.querySelector('.container2');
+
+btn.addEventListener('click', function() {
+
+    if(container2.style.display === 'block'){
+        container2.style.display = 'none';
+    }
+    else{
+        container2.style.display = 'block';
+    }
+
+}); 
+
+var btn = document.querySelector('#show-or-hide3');
+var container3 = document.querySelector('.container3');
+
+btn.addEventListener('click', function() {
+
+    if(container3.style.display === 'block'){
+        container3.style.display = 'none';
+    }
+    else{
+        container3.style.display = 'block';
+    }
+
+}); 
+
+var btn = document.querySelector('#show-or-hide4');
+var container4 = document.querySelector('.container4');
+
+btn.addEventListener('click', function() {
+
+    if(container4.style.display === 'block'){
+        container4.style.display = 'none';
+    }
+    else{
+        container4.style.display = 'block';
+    }
+
+}); 
+
+var btn = document.querySelector('#show-or-hide5');
+var container5 = document.querySelector('.container5');
+
+btn.addEventListener('click', function() {
+
+    if(container5.style.display === 'block'){
+        container5.style.display = 'none';
+    }
+    else{
+        container5.style.display = 'block';
+    }
+
+}); 
+
---
+++ js/timesCampeoes.js
@@ -0,0 +1,296 @@
+var btn = document.querySelector('#show-or-hide');
+var container = document.querySelector('.container');
+
+btn.addEventListener('click', function() {
+
+    if(container.style.display === 'block'){
+        container.style.display = 'none';
+    }
+    else{
+        container.style.display = 'block';
+    }
+
+}); 
+
+var btn = document.querySelector('#show-or-hide2');
+var container2 = document.querySelector('.container2');
+
+btn.addEventListener('click', function() {
+
+    if(container2.style.display === 'block'){
+        container2.style.display = 'none';
+    }
+    else{
+        container2.style.display = 'block';
+    }
+
+}); 
+
+var btn = document.querySelector('#show-or-hide3');
+var container3 = document.querySelector('.container3');
+
+btn.addEventListener('click', function() {
+
+    if(container3.style.display === 'block'){
+        container3.style.display = 'none';
+    }
+    else{
+        container3.style.display = 'block';
+    }
+
+}); 
+
+var btn = document.querySelector('#show-or-hide4');
+var container4 = document.querySelector('.container4');
+
+btn.addEventListener('click', function() {
+
+    if(container4.style.display === 'block'){
+        container4.style.display = 'none';
+    }
+    else{
+        container4.style.display = 'block';
+    }
+
+}); 
+
+var btn = document.querySelector('#show-or-hide5');
+var container5 = document.querySelector('.container5');
+
+btn.addEventListener('click', function() {
+
+    if(container5.style.display === 'block'){
+        container5.style.display = 'none';
+    }
+    else{
+        container5.style.display = 'block';
+    }
+
+}); 
+
+var btn = document.querySelector('#show-or-hide6');
+var container6 = document.querySelector('.container6');
+
+btn.addEventListener('click', function() {
+
+    if(container6.style.display === 'block'){
+        container6.style.display = 'none';
+    }
+    else{
+        container6.style.display = 'block';
+    }
+
+}); 
+
+var btn = document.querySelector('#show-or-hide7');
+var container7 = document.querySelector('.container7');
+
+btn.addEventListener('click', function() {
+
+    if(container7.style.display === 'block'){
+        container7.style.display = 'none';
+    }
+    else{
+        container7.style.display = 'block';
+    }
+
+}); 
+
+var btn = document.querySelector('#show-or-hide8');
+var container8 = document.querySelector('.container8');
+
+btn.addEventListener('click', function() {
+
+    if(container8.style.display === 'block'){
+        container8.style.display = 'none';
+    }
+    else{
+        container8.style.display = 'block';
+    }
+
+}); 
+
+var btn = document.querySelector('#show-or-hide9');
+var container9 = document.querySelector('.container9');
+
+btn.addEventListener('click', function() {
+
+    if(container9.style.display === 'block'){
+        container9.style.display = 'none';
+    }
+    else{
+        container9.style.display = 'block';
+    }
+
+}); 
+
+var btn = document.querySelector('#show-or-hide10');
+var container10 = document.querySelector('.container10');
+
+btn.addEventListener('click', function() {
+
+    if(container10.style.display === 'block'){
+        container10.style.display = 'none';
+    }
+    else{
+        container10.style.display = 'block';
+    }
+
+}); 
+
+var btn = document.querySelector('#show-or-hide11');
+var container11 = document.querySelector('.container11');
+
+btn.addEventListener('click', function() {
+
+    if(container11.style.display === 'block'){
+        container11.style.display = 'none';
+    }
+    else{
+        container11.style.display = 'block';
+    }
+
+}); 
+
+var btn = document.querySelector('#show-or-hide12');
+var container12 = document.querySelector('.container12');
+
+btn.addEventListener('click', function() {
+
+    if(container12.style.display === 'block'){
+        container12.style.display = 'none';
+    }
+    else{
+        container12.style.display = 'block';
+    }
+
+}); 
+
+var btn = document.querySelector('#show-or-hide13');
+var container13 = document.querySelector('.container13');
+
+btn.addEventListener('click', function() {
+
+    if(container13.style.display === 'block'){
+        container13.style.display = 'none';
+    }
+    else{
+        container13.style.display = 'block';
+    }
+
+}); 
+
+var btn = document.querySelector('#show-or-hide14');
+var container14 = document.querySelector('.container14');
+
+btn.addEventListener('click', function() {
+
+    if(container14.style.display === 'block'){
+        container14.style.display = 'none';
+    }
+    else{
+        container14.style.display = 'block';
+    }
+
+}); 
+
+var btn = document.querySelector('#show-or-hide15');
+var container15 = document.querySelector('.container15');
+
+btn.addEventListener('click', function() {
+
+    if(container15.style.display === 'block'){
+        container15.style.display = 'none';
+    }
+    else{
+        container15.style.display = 'block';
+    }
+
+}); 
+
+var btn = document.querySelector('#show-or-hide16');
+var container16 = document.querySelector('.container16');
+
+btn.addEventListener('click', function() {
+
+    if(container16.style.display === 'block'){
+        container16.style.display = 'none';
+    }
+    else{
+        container16.style.display = 'block';
+    }
+
+}); 
+
+var btn = document.querySelector('#show-or-hide17');
+var container17 = document.querySelector('.container17');
+
+btn.addEventListener('click', function() {
+
+    if(container17.style.display === 'block'){
+        container17.style.display = 'none';
+    }
+    else{
+        container17.style.display = 'block';
+    }
+
+}); 
+
+var btn = document.querySelector('#show-or-hide18');
+var container18 = document.querySelector('.container18');
+
+btn.addEventListener('click', function() {
+
+    if(container18.style.display === 'block'){
+        container18.style.display = 'none';
+    }
+    else{
+        container18.style.display = 'block';
+    }
+
+}); 
+
+var btn = document.querySelector('#show-or-hide19');
+var container19 = document.querySelector('.container19');
+
+btn.addEventListener('click', function() {
+
+    if(container19.style.display === 'block'){
+        container19.style.display = 'none';
+    }
+    else{
+        container19.style.display = 'block';
+    }
+
+}); 
+
+var btn = document.querySelector('#show-or-hide20');
+var container20 = document.querySelector('.container20');
+
+btn.addEventListener('click', function() {
+
+    if(container20.style.display === 'block'){
+        container.style.display = 'none';
+    }
+    else{
+        container20.style.display = 'block';
+    }
+
+}); 
+
+var btn = document.querySelector('#show-or-hide21');
+var container21 = document.querySelector('.container21');
+
+btn.addEventListener('click', function() {
+
+    if(container21.style.display === 'block'){
+        container.style.display = 'none';
+    }
+    else{
+        container21.style.display = 'block';
+    }
+
+}); 
+
+
+
